83a99b3a9b3bcc43ece3df50b77a878ba51f9021
ianto3 <33053514+ianto3@users.noreply.github.com>
2021-08-23 16:20:26 +0200

    form rendering and attaching eventlisteners

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,47 @@
 "use strict";
-console.log("hello!");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function autobind(_, _2, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+class ProjectInputs {
+    constructor() {
+        this.templateEl = document.getElementById("project-input");
+        this.hostEl = document.getElementById("app");
+        const importedNode = document.importNode(this.templateEl.content, true);
+        this.formEl = importedNode.firstElementChild;
+        this.formEl.id = "user-input";
+        this.titleInputEl = this.formEl.querySelector("#title");
+        this.descriptionInputEl = this.formEl.querySelector("#description");
+        this.peopleInputEl = this.formEl.querySelector("#people");
+        this.attachEventListener();
+        this.renderForm();
+    }
+    submitHandler(ev) {
+        ev.preventDefault();
+        console.log(this.titleInputEl.value);
+    }
+    attachEventListener() {
+        this.formEl.addEventListener("submit", this.submitHandler);
+    }
+    renderForm() {
+        this.hostEl.insertAdjacentElement("afterbegin", this.formEl);
+    }
+}
+__decorate([
+    autobind
+], ProjectInputs.prototype, "submitHandler", null);
+const invokedFormInput = new ProjectInputs();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,68 @@
-console.log("hello!")
+ // Autobind decorator
+function autobind(
+    _: any, 
+    _2: string, 
+    descriptor: PropertyDescriptor
+    ) {
+ const originalMethod = descriptor.value;
+ const adjDescriptor: PropertyDescriptor = {
+     configurable: true,
+     get(){
+         const boundFn = originalMethod.bind(this);
+         return boundFn;
+     }
+ }
+ return adjDescriptor;
+}
+
+ 
+ class ProjectInputs {
+    templateEl: HTMLTemplateElement;
+    hostEl: HTMLDivElement;
+    formEl: HTMLFormElement;
+    titleInputEl: HTMLInputElement;
+    descriptionInputEl: HTMLInputElement;
+    peopleInputEl: HTMLInputElement;
+
+    constructor() {
+        // Add as HTMLTemplateElement to avoid missing content error.
+        // Same for hostEl.
+        this.templateEl = document.getElementById("project-input")! as HTMLTemplateElement;
+        this.hostEl = document.getElementById("app")! as HTMLDivElement;
+
+        // Selection for rendering form
+        const importedNode = document.importNode(this.templateEl.content, true);
+        this.formEl = importedNode.firstElementChild! as HTMLFormElement;
+        this.formEl.id = "user-input"; // Add id for css styling
+
+        // Access to inputs in form
+        this.titleInputEl = this.formEl.querySelector("#title") as HTMLInputElement;
+        this.descriptionInputEl = this.formEl.querySelector("#description") as HTMLInputElement;
+        this.peopleInputEl = this.formEl.querySelector("#people") as HTMLInputElement;
+
+        // Action to render form
+        this.attachEventListener();
+        this.renderForm();
+    }
+
+    @autobind
+    private submitHandler(ev: Event){
+        ev.preventDefault();
+        console.log(this.titleInputEl.value);
+    }
+
+    private attachEventListener() {
+        // Bind this to the class and avoid it being the event in addEventListener.
+        // Used autobind decorator instead of binding manually next.
+        // this.formEl.addEventListener("submit", this.submitHandler.bind(this));
+        this.formEl.addEventListener("submit", this.submitHandler);
+    }
+
+    // Render form
+    private renderForm(){
+        this.hostEl.insertAdjacentElement("afterbegin", this.formEl);
+    }
+}
+
+// Invoke form
+const invokedFormInput = new ProjectInputs();
